f7f053e36550dea67fe440eebc13b028a5e66610
krishnasuresh156 <72633163+krishnasuresh156@users.noreply.github.com>
2020-11-16 19:45:22 +0530

    Add files via upload

---
+++ Excercise2.py
@@ -0,0 +1,10 @@
+print("Today is good day to learn python")
+print('Python is fun')
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print("Hello" + " World")
+greeting = "Hello"
+#name = "Bruce"
+name = input("Please enter your name: ")
+#If you want to a space, we can add that too
+print(greeting + ' ' + name)
---
+++ Excercise3.py
@@ -0,0 +1,23 @@
+splitString = "This string has been \nsplit over \nseveral \nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+otherSplitString = """This string has been
+split over
+seveal
+lines"""
+print(otherSplitString)
+
+#To print string in same line
+anotherSplitString = """This string has been \
+split over \
+seveal \
+lines"""
+print(anotherSplitString)
+
+print("Number 1\tThe Larch\nNumber 2\tThe Horse Chestnut")
+
+print("C:\\Users\\timbuchalka\\notes.txt")
+print(r"C:\Users\timbuchalka\notes.txt")
